da205231efcc9fc2266fcd53e929b3e60563053c
yusuf963 <mohammad963yusuf@gmail.com>
2021-03-24 21:19:14 +0000

    adding reduce function

--- tree-attribute/fibinnicci.js
+++ tree-attribute/fibinnicci.js
@@ -54,14 +54,14 @@ module.exports = {
   testingFun
 }
 
-// const new1 = [1, 4, 3, 6, 7, -1]
-// const acculator = (arr) => {
-//   arr.reduce((sum, num) => {
-//     const net = sum + num
-//     console.log(net)
-//     return net
-//   }, -5.5)
-// }
-// acculator(new1)
+const new1 = [1, 4, 3, 6, 7, -1]
+const acculator = (arr) => {
+  arr.reduce((sum, num) => {
+    const net = sum + num
+    console.log(net)
+    return net
+  }, -5.5)
+}
+acculator(new1)
 
 
