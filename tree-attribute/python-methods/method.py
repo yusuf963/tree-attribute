8803f842741f1b560a863b033b6d8efaf888eb84
yusuf963 <mohammad963yusuf@gmail.com>
2021-04-03 13:17:05 +0100

    adding

--- tree-attribute/python-methods/method.py
+++ tree-attribute/python-methods/method.py
@@ -6,3 +6,16 @@ print(any(arr))
 
 # return thr binary on the value
 print(bin(1332))
+# work on individual
+print(bool(''))
+print(bytearray(3))
+print(bytes(8))
+
+
+def adding():
+    return 2
+
+
+print(callable(adding))
+# unicode
+print(chr(11))
